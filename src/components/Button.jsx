ad2a10320e6db0f7a04d2f86950752a8c27ca70d
jonghyun <jonghyeon2046@gmail.com>
2021-06-11 18:14:21 +0900

    INIT

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ color, text, onClick }) => {
+	return (
+		<button onClick={onClick} style={{ backgroundColor: color }} className="btn">
+			{text}
+		</button>
+	);
+};
+
+Button.defaultProps = {
+	color: 'steelblue',
+};
+
+Button.propsTypes = {
+	text: PropTypes.string.isRequired,
+	color: PropTypes.string,
+	onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
